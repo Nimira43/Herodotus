3876b20cd90e3dc1486ab3c547ca7ebeebe03910
Nimira43 <n.rabone72@aol.com>
2025-09-20 23:57:17 +0100

    Created and exporting const for site title and description

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Herodotus'
+export const SITE_DESCRIPTION = 'Herodotus is a digital haven for developers. We explore programming, web trends, Linux, and tech culture through curated articles and features.'
