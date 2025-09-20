4b9e24e00dfa955d920885e12407e8016ade8b45
Nimira43 <n.rabone72@aol.com>
2025-09-20 23:59:32 +0100

    Setting ARTICLES_PER_PAGE

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = 'Herodotus'
 export const SITE_DESCRIPTION = 'Herodotus is a digital haven for developers. We explore programming, web trends, Linux, and tech culture through curated articles and features.'
 export const HOMEPAGE_ARTICLE_LIMIT = 7
+export const ARTICLES_PER_PAGE = 6
