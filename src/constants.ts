f74828f76e54f8c7d4b55ea4c5130df1cc87d6b7
Nimira43 <n.rabone72@aol.com>
2025-09-20 23:58:26 +0100

    Setting HOMEPAGE_ARTICLE_LIMIT

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'Herodotus'
 export const SITE_DESCRIPTION = 'Herodotus is a digital haven for developers. We explore programming, web trends, Linux, and tech culture through curated articles and features.'
+export const HOMEPAGE_ARTICLE_LIMIT = 7
